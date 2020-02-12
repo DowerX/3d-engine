3fa9d275c3aa685f4e08b1afcd40ad7902938eb5
Benedek László <41867355+DowerX@users.noreply.github.com>
2020-02-12 21:43:58 +0100

    Initial commit

---
+++ Types/Types.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Types
+{
+    // FLOAT
+
+    public class Vector2f
+    {
+        public float x;
+        public float y;
+
+        public Vector2f(float _x, float _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+
+    public class Vector3f
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Vector3f(float _x, float _y, float _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+    }
+
+    // INT
+
+    public class Vector2i
+    {
+        public int x;
+        public int y;
+
+        public Vector2i(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+    }
+
+    public class Vector3i
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public Vector3i(int _x, int _y, int _z)
+        {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+    }
+}
---
+++ Wavefront/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Types;
+using System.IO;
+
+namespace Wavefront
+{
+    public class OBJ
+    {
+        public List<Vector3f> vertices = new List<Vector3f>();
+        public List<Vector3f> normalCoordinates = new List<Vector3f>();
+        public List<Vector2f> textureCoordinates = new List<Vector2f>();
+        public List<Vector3i> faces = new List<Vector3i>();
+    }
+
+    public class Wavefront
+    {
+        public OBJ LoadOBJ(string path)
+        {
+            string[] file;
+
+            try
+            {
+               file = File.ReadAllLines(path);
+            }
+            catch
+            {
+                Console.WriteLine("Error: Can't read obj file: '" + path + "'.");
+                return null;
+            }
+
+            OBJ obj = new OBJ();
+
+            foreach (string line in file)
+            {
+                string[] parts = line.Split(' ');
+                switch (parts[0]){
+                    case "v":
+
+                        break;
+                }
+            }
+
+            return null;
+        }
+    }
+}
